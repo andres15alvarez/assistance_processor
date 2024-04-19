7c2dde3c43a28b671251441b316b918358fb0b5e
Andres Alvarez <andres15alvarez@gmail.com>
2024-04-19 17:11:41 -0400

    Add docstring to methods

--- models/processor.py
+++ models/processor.py
@@ -5,30 +5,61 @@ from models.student import Student
 
 class Processor:
     def __init__(self) -> None:
+        """
+        Processor of students attendance
+        """
         self.students: dict[str, Student] = {}
 
     def __str__(self) -> str:
         students = self.sort()
         return "\n".join([str(student) for student in students])
 
-    def sort(self):
+    def sort(self) -> list[Student]:
+        """Sort students by minutes in descending order.
+
+        Returns:
+            - list[Student]: list of students sorted.
+        """
         students = list(self.students.values())
         students.sort(reverse=True)
         students = filter(lambda x: x > 4, students)
         return students
 
     def register_student(self, student_name: str):
+        """Register new student.
+
+        Args:
+            - student_name (str): Name of the student to register.
+        """
         student = self.students.get(student_name)
         if student is None:
             self.students[student_name] = Student(student_name)
 
     def convert_hour_str_to_object(self, hour: str) -> timedelta:
+        """Convert hour in string representation to timedelta object.
+
+        The format of the string must be HH:MM to be parsed correctly.
+
+        Args:
+            - hour (str): hour in format HH:MM with 24 hours.
+
+        Returns:
+            - timedelta: time object with the hours and minutes
+        """
         hour_separated = hour.split(":")
         return timedelta(hours=int(hour_separated[0]), minutes=int(hour_separated[1]))
 
     def register_presence(
         self, name: str, day: int, initial_hour: str, final_hour: str
     ):
+        """Register attendance of a student.
+
+        Args:
+            - name (str): Name of the student,
+            - day (int): number of the day of the week from 1 to 7.
+            - initial_hour (str): hour in format HH:MM with 24 hours.
+            - final_hour (str): hour in format HH:MM with 24 hours.
+        """
         student = self.students.get(name)
         if student is None:
             return
--- models/student.py
+++ models/student.py
@@ -3,6 +3,11 @@ from datetime import timedelta
 
 class Student:
     def __init__(self, name: str) -> None:
+        """Student model.
+
+        Args:
+            - name (str): Name of the student.
+        """
         self.name: str = name
         self.days: int = 0
         self.minutes: float = 0
@@ -27,13 +32,31 @@ class Student:
             return self.minutes < other
         return self.minutes < other.minutes
 
-    def calculate_minutes(self, initial_hour: timedelta, final_hour: timedelta):
+    def calculate_minutes(
+        self, initial_hour: timedelta, final_hour: timedelta
+    ) -> float:
+        """Calculate the minutes spend in class.
+
+        Args:
+            - initial_hour (timedelta): hour in which the student come to class.
+            - final_hour (timedelta): hour in which the student out of class.
+
+        Returns:
+            - float: minutes in class.
+        """
         time_in_class = final_hour - initial_hour
         return time_in_class.seconds / 60
 
     def register_presence(
         self, day: int, initial_hour: timedelta, final_hour: timedelta
     ):
+        """Register student time of presence.
+
+        Args:
+            - day (int): number from 1 to 7 indicating day of week.
+            - initial_hour (timedelta): hour in which the student come to class.
+            - final_hour (timedelta): hour in which the student out of class.
+        """
         if day not in self.days_of_week:
             self.days_of_week.append(day)
             self.days += 1
