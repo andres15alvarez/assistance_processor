58627cf338e780417a4db7e067442db9e32784d5
Andres Alvarez <andres15alvarez@gmail.com>
2024-04-18 23:13:07 -0400

    Add processor model to process the data

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import typer
 
+from models.processor import Processor
 
-def main(file: str):
+
+def get_data_from_file(file: str) -> list[str]:
+    data = []
     with open(file) as file:
         lines = file.readlines()
         for line in lines:
-            print(line)
+            data.append(line.strip())
+    return data
+
+
+def process_data(data: list[str]) -> str:
+    processor = Processor()
+    for row in data:
+        row = row.split()
+        if row[0] == "Student":
+            processor.register_student(row[1])
+        elif row[0] == "Presence":
+            processor.register_presence(row[1], row[2], row[3], row[4])
+    return str(processor)
+
+
+def main(file: str):
+    data = get_data_from_file(file)
+    result = process_data(data)
+    print(result)
 
 
 if __name__ == "__main__":
